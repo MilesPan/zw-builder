cd5f19b4602009b4addbd46e231ae745f4289f95
pandongyuan <pandongyuan@aspirecn.com>
2024-06-27 09:51:57 +0800

    fix: 先让问题都问完

--- bin/index.js
+++ bin/index.js
@@ -11,29 +11,32 @@ function runBuildScript() {
     output: process.stdout,
   });
   let needPush = false;
+  let isI18n = false;
   rl.question(`${color.green(`是否需要push到gitlab`)}? (y/n)  `, (res) => {
     rl.close();
     if (res.trim() === "y" || res.trim() === "Y") needPush = true;
-    const buildProcess = spawn(
-      process.platform === "win32" ? "npm.cmd" : "npm",
-      ["run", "build"],
-      { stdio: "inherit" }
-    );
-    buildProcess.on("close", (code) => {
-      if (code === 0) {
-        console.log("打包成功");
-        const rl2 = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout,
-        });
-        rl2.question(`${color.green(`是海外项目吗`)}? (y/n)  `, (res) => {
-          rl2.close();
-          if (res.trim() === "y" || res.trim() === "Y") start(needPush, true);
-          else start(needPush, false);
-        });
-      } else {
-        console.error("打包失败");
-      }
+    const rl2 = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl2.question(`${color.green(`是海外项目吗`)}? (y/n)  `, (res) => {
+      rl2.close();
+      if (res.trim() === "y" || res.trim() === "Y") isI18n = true;
+      else isI18n = false;
+
+      const buildProcess = spawn(
+        process.platform === "win32" ? "npm.cmd" : "npm",
+        ["run", "build"],
+        { stdio: "inherit" }
+      );
+      buildProcess.on("close", (code) => {
+        if (code === 0) {
+          console.log("打包成功");
+          start(needPush, isI18n);
+        } else {
+          console.error("打包失败");
+        }
+      });
     });
   });
 }
